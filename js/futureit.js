273550111bb41c0a7d226471b46a58b1ec7933a5
asif <asifcse22@gmail.com>
2017-11-04 17:37:20 +0600

    Home page update

---
+++ js/futureit.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".client-carousel").owlCarousel({
+  	items:4,
+  	margin: 20,
+  	nav:true,
+  	navText: "",
+  	// autoplay: true,
+  });
+
+
+  $(".experts-carousel").owlCarousel({
+  	items:4,
+  	margin: 20,
+  	nav:true,
+  	navText: "",
+  	// autoplay: true,
+  });
+
+
+});
