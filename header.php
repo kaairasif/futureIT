405bb6a69228f95d50ca4f6775066668d608458f
asif <asifcse22@gmail.com>
2017-11-26 00:44:00 +0600

    Header copy change

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>FUTURE IT</title>
+    <title>FUTURE IT Ltd</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
